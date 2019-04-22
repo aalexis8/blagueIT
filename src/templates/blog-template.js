d543fae19fd976cc5ca86dbfe24b3e26a63d0138
Alex Alexis <AlxAlexis@outlook.com>
2019-04-21 21:02:30 -0400

    scaffolding for pagination rename to blog-template

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
+import { StaticQuery, graphql, Link } from 'gatsby'
 
 import Layout from '../components/layout'
 
@@ -9,6 +9,9 @@ const getMarkdownPosts = graphql`
       totalCount
       edges {
         node {
+          fields {
+            slug
+          }
           id
           frontmatter {
             title
@@ -35,7 +38,9 @@ export default () => (
             {data.allMarkdownRemark.edges.map(({ node }) => (
               <div key={node.id}>
                 <h3>
-                  {node.frontmatter.title}{' '}
+                  <Link to={`/posts${node.fields.slug}`}>
+                    {node.frontmatter.title}
+                  </Link>{' '}
                   <span style={{ color: '#bbb' }}>
                     - {node.frontmatter.date}
                   </span>
