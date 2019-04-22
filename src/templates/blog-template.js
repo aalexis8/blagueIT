611c7f0fe33160208c8b656db6c48486f50a73f8
Alex Alexis <AlxAlexis@outlook.com>
2019-04-21 21:24:52 -0400

    pagination complete, dynamically setting prev next

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -1,11 +1,56 @@
 import React from 'react'
-import { StaticQuery, graphql, Link } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 
 import Layout from '../components/layout'
 
-const getMarkdownPosts = graphql`
-  {
-    allMarkdownRemark {
+export default ({ data, pageContext }) => {
+  const { currentPage, isFirstPage, isLastPage } = pageContext
+  const nextPage = `/blog/${String(currentPage + 1)}`
+  const prevPage =
+    currentPage - 1 === 1 ? '/blog' : `/blog/${String(currentPage - 1)}`
+
+  return (
+    <Layout>
+      <div>
+        <h1 style={{ display: 'inlineBlock', borderBottom: '1px solid' }}>
+          IT Blague Blog
+        </h1>
+
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <h3>
+              <Link to={`/posts${node.fields.slug}`}>
+                {node.frontmatter.title}
+              </Link>{' '}
+              <span style={{ color: '#bbb' }}>- {node.frontmatter.date}</span>
+            </h3>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
+
+        {/* Pagination Links */}
+        <div>
+          {!isFirstPage && (
+            <Link to={prevPage} rel="prev">
+              Prev Page
+            </Link>
+          )}
+
+          {!isLastPage && (
+            <Link to={nextPage} rel="next">
+              Next Page
+            </Link>
+          )}
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query($skip: Int!, $limit: Int!) {
+    allMarkdownRemark(skip: $skip, limit: $limit) {
       totalCount
       edges {
         node {
@@ -23,34 +68,3 @@ const getMarkdownPosts = graphql`
     }
   }
 `
-
-export default () => (
-  <Layout>
-    <div>
-      <h1 style={{ display: 'inlineBlock', borderBottom: '1px solid' }}>
-        IT Blague Blog
-      </h1>
-      <StaticQuery
-        query={getMarkdownPosts}
-        render={data => (
-          <>
-            <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
-            {data.allMarkdownRemark.edges.map(({ node }) => (
-              <div key={node.id}>
-                <h3>
-                  <Link to={`/posts${node.fields.slug}`}>
-                    {node.frontmatter.title}
-                  </Link>{' '}
-                  <span style={{ color: '#bbb' }}>
-                    - {node.frontmatter.date}
-                  </span>
-                </h3>
-                <p>{node.excerpt}</p>
-              </div>
-            ))}
-          </>
-        )}
-      />
-    </div>
-  </Layout>
-)
