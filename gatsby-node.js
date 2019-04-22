251eb361e05416bfaea2d481295b3c3e76ec3a4e
Alex Alexis <AlxAlexis@outlook.com>
2019-04-21 20:30:45 -0400

    programmatically creating pages for markdown files

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,7 @@
+const path = require('path')
+const { createFilePath } = require('gatsby-source-filesystem')
+
+const PostTemplate = path.resolve('./src/templates/post-template.js')
 /**
  * Implement Gatsby's Node APIs in this file.
  *
@@ -5,3 +9,42 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === 'MarkdownRemark') {
+    const slug = createFilePath({ node, getNode, basePath: 'posts' })
+    createNodeField({
+      node,
+      name: 'slug',
+      value: slug,
+    })
+  }
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+  const posts = result.data.allMarkdownRemark.edges
+  posts.forEach(({ node: post }) => {
+    createPage({
+      path: `posts${post.fields.slug}`,
+      component: PostTemplate,
+      context: {
+        slug: post.fields.slug,
+      },
+    })
+  })
+}
